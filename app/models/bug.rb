cdc10e160745f3129544ad4fd95480d6cf8cfc1d
[ZachArens] <[arens.zach@gmail.com]>
2020-03-17 21:21:17 -0400

    added data validation to model

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,3 +1,10 @@
 class Bug < ApplicationRecord
+  enum issue_type: [:issue, :enhancement, :feature]
+  enum priority: [:low, :medium, :high]
+  enum status: [:open, :closed, :monitor]
+
+  validates :title,  presence: true, length: {minimum: 5, maximum: 64}
+  validates :description,  presence: true, length: {minimum: 20, maximum: 600}
+
   belongs_to :user
 end
